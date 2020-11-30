06ac9ba6bfa9c7583c8e89f1707da8e72d70e555
trumpeta19 <48192194+trumpeta19@users.noreply.github.com>
2020-11-30 16:48:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Digits_of_e
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int MAX = 14;
+            int digits;
+
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter digits of e to print (" + MAX + " or less)");
+
+                if (int.TryParse(Console.ReadLine(), out digits))
+                {
+                    if (digits > MAX)
+                    {
+                        Console.WriteLine(Math.Round(Math.E, MAX));
+                    }
+                    else if (digits < 0)
+                    {
+                        Console.WriteLine(Math.Round(Math.E, 0));
+                    }
+                    else
+                    {
+                        Console.WriteLine(Math.Round(Math.E, digits));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Entry");
+                }
+
+                Console.WriteLine("Press any key to continue or 'Esc' to exit program");
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        }
+    }
+}
